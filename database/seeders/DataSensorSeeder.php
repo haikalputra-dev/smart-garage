37cf0022e25f3152c20b5a5efa0947544d1df53c
haikalputra-dev <mulyaputrahaikal@gmail.com>
2024-06-28 19:50:58 +0700

    done lagi

--- database/seeders/DataSensorSeeder.php
+++ database/seeders/DataSensorSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use App\Models\LogGarasi;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DataSensorSeeder extends Seeder
@@ -17,5 +19,19 @@ class DataSensorSeeder extends Seeder
         $garasi->id_garasi= 1;
         $garasi->status='Terkunci';
         $garasi->save();
+
+        // Sample data to insert
+        $data = [
+            [
+                'id_garasi' => 1,
+                'suhu' => 0,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            // Add more entries as needed
+        ];
+
+        // Insert data into the database
+        DB::table('data_sensor')->insert($data);
     }
 }
